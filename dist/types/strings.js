0f975da1251706402bfde7d42c7da39f314d6966
Marcelo Marques de Lima.dev@outlook.com <marcelomarqueslima.dev@outlook.com>
2023-12-21 07:13:50 -0300

    Estudando tipo no TS

---
+++ dist/types/strings.js
@@ -0,0 +1,8 @@
+"use strict";
+let primeiroNome;
+let segundoNome = 'Marques';
+primeiroNome = 'Marcelo';
+const nomeCompleto = `${primeiroNome} ${segundoNome}`;
+console.log(nomeCompleto);
+console.log(nomeCompleto.toUpperCase());
+console.log(typeof nomeCompleto);
---
+++ types/strings.ts
@@ -0,0 +1,9 @@
+let primeiroNome: string;
+let segundoNome: string = 'Marques';
+
+primeiroNome = 'Marcelo';
+
+const nomeCompleto = `${primeiroNome} ${segundoNome}`;
+console.log(nomeCompleto);
+console.log(nomeCompleto.toUpperCase());
+console.log(typeof nomeCompleto);
